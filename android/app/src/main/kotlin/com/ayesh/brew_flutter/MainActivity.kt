9d8e2b6f5d1497cb98576088b815bae8ee4a2a6d
ayesh <ayeshlak1998@gmail.com>
2021-04-19 16:29:08 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/ayesh/brew_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ayesh.brew_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/auth.dart
@@ -0,0 +1,15 @@
+class AuthService {
+  // final FirebaseAuth _auth = FirebaseAuth.instance;
+  //
+  // //signIn anonymously
+  //
+  // Future signInAnon() async {
+  //   try {
+  //     UserCredential result = await _auth.signInAnonymously();
+  //     return result.user;
+  //   } catch (e) {
+  //     print(e.toString());
+  //     return null;
+  //   }
+  // }
+}
